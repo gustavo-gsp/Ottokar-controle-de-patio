726c01a1d36527aee9e3ebe3d6167a545aa4252f
gustavo-gsp <gustavo.s.pinho@hotmail.com>
2023-07-22 08:19:32 -0300

    puppeteer no-sandbox

--- controller/CarController.js
+++ controller/CarController.js
@@ -460,6 +460,7 @@ try{
     const plate = req.params.plate.toUpperCase();
     const browser = await puppeteer.launch({
         headless: 'new',
+        args: ['--no-sandbox'],
         executablePath: '/opt/google/chrome/chrome'
     });
     const page = await browser.newPage();
