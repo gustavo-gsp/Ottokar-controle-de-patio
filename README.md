6a812d804c7f6d7ce69e965cb9d9f6fc0edc0b60
gustavo-gsp <gustavo.s.pinho@hotmail.com>
2023-08-22 00:00:11 -0300

    correction to card order parts

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
 # Ottokar-controle-de-patio
-
-if(user != undefined){
-    let correct = Bcrypt.compareSync(log.password, user.password);
-    if(correct){
-        userName = name; 
-        userFunc = user.func; 
-        
-        res.redirect('/carPage/today/a')
-    }else{
-        message = 'Usúario ou senha inválidos!';
-        res.render('login', {message});
-    }
-}else{
-    message = 'Usúario ou senha inválidos!';
-    res.render('login', {message});
-}    
-});
--- controller/CarController.js
+++ controller/CarController.js
@@ -702,7 +702,7 @@ const orderParts = async (req, res) =>{
         if (car.parts) {
             parts = [...car.parts];
         }
-        if (req.body.servicos) {
+        if (req.body.servicos != null) {
             req.body.servicos.forEach((serv, index) =>{
                 servicesUpd[index] = {
                     service: serv,
@@ -712,7 +712,7 @@ const orderParts = async (req, res) =>{
             servicesUpd.push(end)
             services.push(...servicesUpd);
         }
-        if (req.body.pecas) {
+        if (req.body.pecas != null) {
             req.body.pecas.forEach((part, index) =>{
                 partsUpd[index] = {
                     part: part,
